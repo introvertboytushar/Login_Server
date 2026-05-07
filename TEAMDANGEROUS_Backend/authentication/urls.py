86b16e805ce48579dfb3a13b87fe5b2d435b9d93
introvertboytushar <introvertboytushar@gmail.com>
2026-05-07 13:18:20 +0600

    Update urls.py

--- TEAMDANGEROUS_Backend/authentication/urls.py
+++ TEAMDANGEROUS_Backend/authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, VerifyView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('verify/', VerifyView.as_view(), name='verify'),
 ]
