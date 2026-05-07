e49f2a55c5afaed602faa6cc89e159e0e6e4460d
introvertboytushar <introvertboytushar@gmail.com>
2026-05-07 13:58:36 +0600

    Update admin.py

--- TEAMDANGEROUS_Backend/authentication/admin.py
+++ TEAMDANGEROUS_Backend/authentication/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
-
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'phone', 'date_joined', 'is_active')
+    list_display = ('username', 'first_name', 'email', 'phone', 'date_joined', 'is_active')
     list_filter = ('is_active', 'is_staff', 'date_joined')
-    search_fields = ('username', 'email', 'phone')
+    search_fields = ('username', 'email', 'phone', 'first_name')
     ordering = ('-date_joined',)
     
     fieldsets = UserAdmin.fieldsets + (
