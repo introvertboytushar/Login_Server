754a4e265277f0235728cb3cbe5840bdfb4f1fb1
introvertboytushar <introvertboytushar@gmail.com>
2026-05-07 13:10:40 +0600

    Set custom user model for authentication

--- TEAMDANGEROUS_Backend/core/settings.py
+++ TEAMDANGEROUS_Backend/core/settings.py
@@ -84,3 +84,4 @@ CORS_ALLOW_HEADERS = ['*']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+AUTH_USER_MODEL = 'authentication.CustomUser'  # ← 
