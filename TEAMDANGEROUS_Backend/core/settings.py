bcb2085c1f3b52f303471cdcf9774f541b6a52a5
introvertboytushar <introvertboytushar@gmail.com>
2026-05-07 11:16:45 +0600

    Update settings.py

--- TEAMDANGEROUS_Backend/core/settings.py
+++ TEAMDANGEROUS_Backend/core/settings.py
@@ -40,11 +40,14 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'core.urls'
 WSGI_APPLICATION = 'core.wsgi.application'
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 REST_FRAMEWORK = {
