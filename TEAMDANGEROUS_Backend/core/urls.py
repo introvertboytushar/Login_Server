7b96bdcf925b9bc02a208bbdffea204f10e7f61b
introvertboytushar <introvertboytushar@gmail.com>
2026-05-07 12:06:00 +0600

    Update urls.py

--- TEAMDANGEROUS_Backend/core/urls.py
+++ TEAMDANGEROUS_Backend/core/urls.py
@@ -3,3 +3,10 @@ from django.urls import path, include
 urlpatterns = [
     path('api/', include('authentication.urls')),
 ]
+from django.urls import path, include
+from django.contrib import admin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('authentication.urls')),
+]
